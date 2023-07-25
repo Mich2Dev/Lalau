36cfb3940bc46e164152703e15c2d418c0abafd3
Mich <mencocuellomaicol@gmail.com>
2023-07-25 00:02:23 -0500

    inicio del manejo estructural de rutas

---
+++ Project_Accesories/config/rutas.php
@@ -0,0 +1,9 @@
+<?php
+    class Rutas
+        {
+        public static function Cargar($control,$accion){
+           
+            print($control);
+        }
+    }
+?>
--- Project_Accesories/index.php
+++ Project_Accesories/index.php
@@ -1,9 +1,16 @@
 <?php
-    if (isset($_GET["control"]) && isset($_GET["accion"])) {
+    require_once "config/rutas.php";
+
+    if (isset($_GET["control"]) || isset($_GET["accion"])) 
+    {
         $control = $_GET["control"];
         $accion = $_GET["accion"];
-    } else {
+    }
+     else 
+    {
         $control = "init";
         $accion = "index";
     }
+    
+    Rutas::Cargar($control,$accion);
 ?>
