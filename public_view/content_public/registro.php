6b3a4d2329d1151a955bb4ab7120e48c2d5758b7
Mich2dev <mencocuellomaicol@gmail.com>
2023-11-26 15:21:38 -0500

    segundo trabajo sobre el modulo de registro

--- public_view/content_public/registro.php
+++ public_view/content_public/registro.php
@@ -3,88 +3,108 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro Rosa</title>
+    <title>Registro Personalizado</title>
     <!-- Incluye Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Incluye Font Awesome para los iconos -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <!-- Incluye la fuente de Google, Montserrat -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat&display=swap">
     <!-- Estilos personalizados -->
     <style>
         body {
-            background-color: #f8d7da; /* Color de fondo rosa claro */
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #ffd4e5; /* Color de fondo rosa claro */
+            margin: 0;
+            font-family: 'Montserrat', sans-serif; /* Aplica la fuente de Google */
         }
 
         .container {
-            margin-top: 50px;
-        }
-
-        .card {
-            border: none;
-            border-radius: 15px;
+            background-color: #ffffff;
+            padding: 40px;
+            border-radius: 10px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            text-align: center;
         }
 
-        .card-header {
-            background-color: #dc3545; /* Color de encabezado rosa oscuro */
-            color: #fff;
-            text-align: center;
-            border-radius: 15px 15px 0 0;
+        h4 {
+            color: #dc3545; /* Color de encabezado rosa oscuro */
+            margin-bottom: 20px;
+            font-size: 24px; /* Tamaño de fuente personalizado */
         }
 
         .form-group {
             margin-bottom: 20px;
         }
 
-        .btn-pink {
-            background-color: #dc3545;
-            color: #fff;
+        input.form-control {
+            font-family: 'Montserrat', sans-serif; /* Aplica la fuente de Google a los campos de entrada */
         }
 
-        .btn-pink:hover {
-            background-color: #c82333;
+        .btn-custom {
+            background-color: #dc3545; /* Color de botón rosa oscuro */
+            color: #fff;
+            transition: background-color 0.3s;
+            font-family: 'Montserrat', sans-serif; /* Aplica la fuente de Google al botón */
+        }
+
+        .btn-custom:hover {
+            background-color: #c82333; /* Color de botón rosa más oscuro al pasar el mouse */
+        }
+
+        .btn-custom:active {
+            background-color: #28a745; /* Color de botón verde cuando se presiona */
+        }
+
+        .botoregreso {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+            background-color: #80b3ff;
+            color: #ffffff;
+            font-size: 16px;
+            text-decoration: none;
+            margin: 20px auto;
+            font-family: 'Montserrat', sans-serif; /* Aplica la fuente de Google al botón de regreso */
         }
     </style>
 </head>
 <body>
 
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">
-                    <h4>Registro</h4>
-                </div>
-                <div class="card-body">
-                    <!-- Formulario de registro -->
-                    <form>
-                        <div class="form-group">
-                            <label for="nombre">Nombre</label>
-                            <input type="text" class="form-control" id="nombre" placeholder="Ingresa tu nombre" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="correo">Usuario</label>
-                            <input type="user" class="form-control" id="usuario" placeholder="Ingresa tu correo electrónico" required>
-                        </div>
-                        <div class="form-group">
-                          <label for="correo">N° Telefono</label>
-                          <input type="number" class="form-control" id="Telefono" placeholder="Ingresa tu correo electrónico" required>
-                      </div>
-                        <div class="form-group">
-                          <label for="correo">Correo electrónico</label>
-                          <input type="email" class="form-control" id="correo" placeholder="Ingresa tu correo electrónico" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="correo">Contraseña</label>
-                            <input type="password" class="form-control" id="correo" placeholder="Ingresa tu correo electrónico" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="contrasena">N.Contraseña</label>
-                            <input type="password" class="form-control" id="contrasena" placeholder="Ingresa tu contraseña" required>
-                        </div>
-                        <button type="submit" class="btn btn-pink btn-block">Registrar</button>
-                    </form>
-                </div>
-            </div>
+    <h4>Registro</h4>
+    <!-- Formulario de registro -->
+    <form>
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Nombre Completo" required>
         </div>
-    </div>
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Nombre de Usuario" required>
+        </div>
+        <div class="form-group">
+            <input type="tel" class="form-control" placeholder="Número de Teléfono" required>
+        </div>
+        <div class="form-group">
+            <input type="email" class="form-control" placeholder="Correo Electrónico" required>
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" placeholder="Contraseña" required>
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" placeholder="Confirmar Contraseña" required>
+        </div>
+        <button type="submit" class="btn btn-custom btn-block">Registrar</button>
+        <!-- Botón de regreso -->
+        <a href="login" class="botoregreso">
+            <i class="fas fa-arrow-left"></i>
+        </a>
+    </form>
 </div>
 
 <!-- Incluye Bootstrap JS y Popper.js -->
